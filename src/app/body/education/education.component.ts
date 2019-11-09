7f0957439fd1c9524731bc9907f92d878c895408
Rojesh Dangol <rxd1026@sru.com>
2019-11-08 21:05:53 -0500

    webpage html codes were added and working

---
+++ src/app/body/education/education.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.css']
+})
+export class EducationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
