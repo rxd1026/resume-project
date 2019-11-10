88307a0a3612efd510c4f5db39dc2dffffe97bf5
Rojesh Dangol <rxd1026@sru.com>
2019-11-09 23:14:29 -0500

    codes edited and skill added

---
+++ src/app/body/skill/skill.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-skill',
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.css']
+})
+export class SkillComponent implements OnInit {
+ 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
